9f23e05733424e402c372e0d97d026c8e9ce4027
zhujiapeng <zhujiapeng@gmail.com>
2022-03-15 00:45:33 +0800

    第一次提交：实现了首页功能

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,41 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * @Author: ZhuJiapeng
+ * @Description:
+ * @Date: Created in 15:37 2021/12/26
+ * @Modified By:
+ */
+@Service
+//@Scope("prototype") //原型，表示通过 Spring 容器获取的对象是不同的
+public class AlphaService {
+
+    @Autowired
+    private AlphaDao alphaDao;
+
+    public AlphaService() {
+        System.out.println("实例化AlphaService");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化AlphaService");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("销毁AlphaService");
+    }
+
+    public String find(){
+        return alphaDao.select();
+    }
+}
